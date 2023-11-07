a34df278d5db3e6a5c47d1629e1f0ae08ab07022
klaus <118160573+redaezziani@users.noreply.github.com>
2023-11-07 15:29:54 +0100

    aquarium page �

---
+++ aquarium/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div
+    className="w-full flex relative  justify-start items-center flex-col min-h-screen"
+    >
+      <h1>
+        Hello World!
+      </h1>
+    </div>
+  )
+}
+
+export default App
