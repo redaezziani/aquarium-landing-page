017166ff313206b22ad7c3ff00e265ae539d3ebb
redaezziani <2003100500167@eofppt-edu.ma>
2023-11-10 09:02:53 +0100

    add

--- aquarium/src/components/BolbEffects.tsx
+++ aquarium/src/components/BolbEffects.tsx
@@ -6,6 +6,110 @@ import {motion} from "framer-motion"
 const BolbEffects = () => {
     const [posX, setPosX]=useState(0)
     const [posY, setPosY]=useState(0)
+    const color=[{
+        bg:"bg-red-500",
+        text:"text-red-500"
+    },
+    {
+        bg:"bg-blue-500",
+        text:"text-blue-500"
+    },
+    {
+        bg:"bg-green-500",
+        text:"text-green-500"
+    },
+    {
+        bg:"bg-yellow-500",
+        text:"text-yellow-500"
+    },
+    {
+        bg:"bg-indigo-500",
+        text:"text-indigo-500"
+    },
+    {
+        bg:"bg-pink-500",
+        text:"text-pink-500"
+    },
+    {
+        bg:"bg-purple-500",
+        text:"text-purple-500"
+    },
+    {
+        bg:"bg-gray-500",
+        text:"text-gray-500"
+    },
+    {
+        bg:"bg-slate-500",
+        text:"text-slate-500"
+    },
+    {
+        bg:"bg-cyan-500",
+        text:"text-cyan-500"
+    },
+    {
+        bg:"bg-emerald-500",
+        text:"text-emerald-500"
+    },
+    {
+        bg:"bg-lime-500",
+        text:"text-lime-500"
+    },
+    {
+        bg:"bg-rose-500",
+        text:"text-rose-500"
+    },
+    {
+        bg:"bg-amber-500",
+        text:"text-amber-500"
+    },
+    {
+        bg:"bg-orange-500",
+        text:"text-orange-500"
+    },
+    {
+        bg:"bg-teal-500",
+        text:"text-teal-500"
+    },
+    {
+        bg:"bg-cyan-500",
+        text:"text-cyan-500"
+    },
+    {
+        bg:"bg-fuchsia-500",
+        text:"text-fuchsia-500"
+    },
+    {
+        bg:"bg-violet-500",
+        text:"text-violet-500"
+    },
+    {
+        bg:"bg-lightBlue-500",
+        text:"text-lightBlue-500"
+    }
+] 
+
+const color2=[
+    "text-red-500/40",
+    "text-blue-500/40",
+    "text-green-500/40",
+    "text-yellow-500/40",
+    "text-indigo-500/40",
+    "text-pink-500/40",
+    "text-purple-500/40",
+    "text-gray-500/40",
+    "text-slate-500/40",
+    "text-cyan-500/40",
+    "text-emerald-500/40",
+    "text-lime-500/40",
+    "text-rose-500/40",
+    "text-amber-500/40",
+    "text-orange-500/40",
+    "text-teal-500/40",
+    "text-cyan-500/40",
+    "text-fuchsia-500/40",
+    "text-violet-500/40",
+    "text-lightBlue-500/40"
+]
 
     const addBorderToTheTargetElement=(e)=>{
         e.target.style.border="1.5px solid black"
@@ -15,10 +119,33 @@ const BolbEffects = () => {
         e.target.style.borderStyle="dashed"
     }
     
-    const getPosition=(e)=>{
-        setPosX(e.clientX)
-        setPosY(e.clientY)
+
+    const [colorIndex, setColorIndex]=useState(0)
+    const [colorIndex2, setColorIndex2]=useState(0)
+    const changeColor=()=>{
+        if(colorIndex===color.length-1){
+            setColorIndex(0)
+        }else{
+            setColorIndex(colorIndex+1)
+        }
     }
+
+    const changeColor2=()=>{
+        if(colorIndex2===color2.length-1){
+            setColorIndex2(0)
+        }else{
+            setColorIndex2(colorIndex2+1)
+        }
+    }
+
+    setTimeout(() => {
+        changeColor2()
+    }, 2000);
+
+
+    setTimeout(() => {
+        changeColor()
+    }, 2000);
 
     useEffect(()=>{
     
@@ -34,13 +161,13 @@ const BolbEffects = () => {
         setPosY(e.clientY)
     }}
     onClick={(e)=>addBorderToTheTargetElement(e)}
-    className="flex border-red-500 text-slate-800 mt-32 justify-start items-center flex-col gap-4 text-lg"
+    className={`flex   mt-32 justify-start items-center flex-col gap-4 text-lg font-bold   ${color[colorIndex].text} p-4 rounded-xl`}
     >
         <motion.h2
         drag={true}
         dragElastic={0.5}
         dragConstraints={{left:0,right:0,top:0,bottom:0}}
-        className=" text-3xl font-bold text-slate-900 capitalize"
+        className=" text-3xl font-bold  capitalize"
         >
         pegasus is the best builder web site 
         </motion.h2>
@@ -48,7 +175,7 @@ const BolbEffects = () => {
         drag={true}
         dragElastic={0.5}
         dragConstraints={{left:0,right:0,top:0,bottom:0}}
-        className=" text-slate-500 text-sm"
+        className={` text-slate-500 text-sm font-bold  ${color2[colorIndex2]} p-4 rounded-xl`}
         >
             drag and drop all the element in this web site just try it, just have fun 
         </motion.p>
